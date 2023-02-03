94702212e7139a3b10b3be6776ce023037e84ab5
gin7775 <alfonsogianmarco@gmail.com>
2023-02-04 00:04:32 +0100

    Movimiento important
    aqui ta

--- Assets/Scripts/MovementCamera.cs
+++ Assets/Scripts/MovementCamera.cs
@@ -9,20 +9,38 @@ public class MovementCamera : MonoBehaviour
 {
     public float forwardSpeed, strafeSpeed, hoverSpeed;
     private float activeStrafeSpeed, activeHoverSpeed;
+    public float fowardAcceleration = 2.5f, strafeAcceleration=2f, hoverAcceleration=2f;
+
+    public float lookRateSpeed = 90f;
+    private Vector2 lookInput, screenCenter, mouseDistance;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        screenCenter.x = Screen.width * .5f;
+        screenCenter.y = Screen.height * .5f;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        activeStrafeSpeed = Input.GetAxisRaw("Horizontal") + strafeSpeed;
-        activeHoverSpeed = Input.GetAxisRaw("Hover") + hoverSpeed;
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
+
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
+
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime,0f , Space.Self);
+
+
+       /* activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") + strafeSpeed,strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp( activeHoverSpeed,Input.GetAxisRaw("Hover") + hoverSpeed,hoverAcceleration * Time.deltaTime);*/
 
         transform.position += transform.forward * forwardSpeed * Time.deltaTime;
+       // transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
+
     }
 }
