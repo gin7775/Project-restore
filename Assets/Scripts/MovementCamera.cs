a423f6d7a864c28a9a4016c0eba721e125bf91bb
gin7775 <alfonsogianmarco@gmail.com>
2023-02-04 09:23:22 +0100

    xg

--- Assets/Scripts/MovementCamera.cs
+++ Assets/Scripts/MovementCamera.cs
@@ -11,7 +11,7 @@ public class MovementCamera : MonoBehaviour
     private float activeStrafeSpeed, activeHoverSpeed;
     public float fowardAcceleration = 2.5f, strafeAcceleration=2f, hoverAcceleration=2f;
     public float rootLimit;
-    public Camera camara;
+    public GameObject camara;
 
     public float lookRateSpeed = 90f;
     private Vector2 lookInput, screenCenter, mouseDistance;
@@ -35,16 +35,16 @@ public class MovementCamera : MonoBehaviour
 
         mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
 
-        float posRot = -mouseDistance.y * lookRateSpeed * Time.deltaTime;
+        //float posRot = -mouseDistance.y * lookRateSpeed * Time.deltaTime;
         float negRot = mouseDistance.x* lookRateSpeed *Time.deltaTime;
-
-        
+        float posRot = 0;
+       
         transform.Rotate(posRot, negRot, 0f, Space.Self);
 
-        //LimitRoot();
+        LimitRoot();
 
-        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") + strafeSpeed,strafeAcceleration * Time.deltaTime);
-        activeHoverSpeed = Mathf.Lerp( activeHoverSpeed,Input.GetAxisRaw("Vertical") + hoverSpeed,hoverAcceleration * Time.deltaTime);
+        activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed,strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp( activeHoverSpeed,Input.GetAxisRaw("Vertical") * hoverSpeed,hoverAcceleration * Time.deltaTime);
 
         transform.position += transform.forward * forwardSpeed * Time.deltaTime;
         transform.position += (transform.right * activeStrafeSpeed * Time.deltaTime) + (transform.up * activeHoverSpeed * Time.deltaTime);
@@ -55,9 +55,9 @@ public class MovementCamera : MonoBehaviour
     {
 
         Vector3 playerEulerAngles = transform.rotation.eulerAngles;
-        /*
+        
         playerEulerAngles.y = (playerEulerAngles.y > 180) ? playerEulerAngles.y - 360 : playerEulerAngles.y;
-        playerEulerAngles.y = Mathf.Clamp(playerEulerAngles.y, rootLimit * -1, rootLimit);*/
+        playerEulerAngles.y = Mathf.Clamp(playerEulerAngles.y, rootLimit * -1, rootLimit);
 
         playerEulerAngles.x = (playerEulerAngles.x > 180) ? playerEulerAngles.x - 360 : playerEulerAngles.x;
         playerEulerAngles.x = Mathf.Clamp(playerEulerAngles.x, rootLimit * -1, rootLimit);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float forwardSpeed = 20;
     public GameObject targetF,cameraObject;
+    public float lookRateSpeed = 90f;
+    private Vector2 lookInput, screenCenter, targetDistance;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = targetF.transform.position;
-        transform.rotation = cameraObject.transform.rotation;
+        lookInput.x = targetF.transform.position.x;
+        lookInput.y = targetF.transform.position.y;
+
+        targetDistance.x = (lookInput.x - transform.position.x) / transform.position.y;
+        targetDistance.y = (lookInput.y - transform.position.y) / transform.position.y;
+
+        targetDistance = Vector2.ClampMagnitude(targetDistance, 1f);
+
+        /*transform.position = targetF.transform.position;
+        transform.rotation = cameraObject.transform.rotation;*/
+
+        float posRot = -targetDistance.y * lookRateSpeed * Time.deltaTime;
+        float negRot = targetDistance.x * lookRateSpeed * Time.deltaTime;
+
+        transform.Rotate(posRot, negRot, 0f, Space.Self);
+
+        transform.position += transform.forward * forwardSpeed * Time.deltaTime;
+
+
     }
 }
