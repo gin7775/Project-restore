81b74f78b2d00a2d2f8a40f61671c3882b37cd9e
merylol22 <merylol22@gmail.com>
2023-02-05 13:30:57 +0100

    Opciones

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -17,10 +17,32 @@ public class MainMenu : MonoBehaviour
         
     }
 
-    public void SceneGame()
+    public void SceneGame(int sceneToChange)
     {
-        SceneManager.LoadScene("Juego");
+        
+        switch (sceneToChange)
+        {
+            case 1:
+                SceneManager.LoadScene("Blocking");
+                break;
+            case 2:
+                SceneManager.LoadScene("Opciones");
+                break;
+            case 3:
+                SceneManager.LoadScene("Credits");
+                break;
+            case 4:
+                SceneManager.LoadScene("Menu");
+                break;
+            case 5:
+                Application.Quit();
+                break;
+
+        }
+        
     }
+
+
 
     public void CargarNivel(string nombreNivel)
     {
